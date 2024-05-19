0ed787ae813a2a396bf87d570a2a7c458e08040e
Taronbochi <shiyouban@gmail.com>
2024-05-19 10:28:39 +0900

    app.js の更新

--- app.js
+++ app.js
@@ -1,16 +1,3 @@
-// Firebase configuration
-const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-    databaseURL: "https://YOUR_PROJECT_ID.firebaseio.com",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_PROJECT_ID.appspot.com",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID"
-};
-
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
 const db = firebase.database();
 const shoppingListRef = db.ref('shoppingList');
 
